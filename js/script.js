6e3b7230477c6a7bdd854b0f22e0b49b01a45c14
iDammer12 <146379225+iDammer12@users.noreply.github.com>
2023-10-19 18:48:21 +0300

    fixed slideshow bug

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ let value = 1;
 function addOne() {
   value += 1;
   if(value === 6) {
-    value === 1
+    value = 1
   }
-  console.log(value)
   document.querySelector('.slideshow').innerHTML = `
   <img class="slideshow-element" src="images/product-${value}.webp" alt="">
   `
 }
 
-setInterval(addOne, 2000);
+setInterval(addOne, 3000);
