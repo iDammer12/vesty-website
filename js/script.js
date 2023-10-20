366b4a6dbda2a2500e70568935877175e7eab9e4
iDammer12 <146379225+iDammer12@users.noreply.github.com>
2023-10-20 23:04:34 +0300

    made 2nd product

--- js/script.js
+++ js/script.js
@@ -11,3 +11,38 @@ function addOne() {
 }
 
 setInterval(addOne, 3000);
+
+const product = [{
+  image: "images/jordan-1-black-orange.webp", 
+  name: "Air Jordan 1 Low", 
+  price: 2199
+}, {
+  image: "images/air-jordan-1-low-black-lucky-green-3.png", 
+  name: "Jordan AJ 1 Low", 
+  price: 2199
+}];
+
+
+let productsHTML = ''
+
+product.forEach((product) => {
+
+  productsHTML +=`
+  <div class="square">
+    <div class="product-image">
+      <img src="${product.image}" alt="">
+    </div>
+    <div class="product-title">
+      ${product.name}
+    </div>
+    <div class="product-price">
+      ${product.price} MDL <div>-30%</div>
+    </div>
+    <div class="product-details">
+      <button>Detalii</button>
+    </div>
+  </div>
+  `
+})
+
+document.querySelector('.grid-container').innerHTML = productsHTML
