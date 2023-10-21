b393257dbe5e9259c533383fb95353bc5486ab51
iDammer12 <146379225+iDammer12@users.noreply.github.com>
2023-10-21 19:17:14 +0300

    redesigned

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function addOne() {
     value = 1
   }
   document.querySelector('.slideshow').innerHTML = `
-  <img class="slideshow-element" src="images/product-${value}.webp" alt="">
+  <img class="slideshow-element" src="images/slideshow-products/${value}.jpg" alt="">
   `
 }
 
@@ -15,11 +15,20 @@ setInterval(addOne, 3000);
 const product = [{
   image: "images/jordan-1-black-orange.webp", 
   name: "Air Jordan 1 Low", 
-  price: 2199
+  price: 2199,
+  sale: "<div>-30%</div>"
 }, {
   image: "images/air-jordan-1-low-black-lucky-green-3.png", 
   name: "Jordan AJ 1 Low", 
-  price: 2199
+  price: 2199, 
+  sale: "<div></div>",
+  style: "height: 65px; margin-top: 10px;"
+}, {
+  image: "images/nike-air-force-1-07-white.png", 
+  name: "Air Force 1 07'", 
+  price: 2299 ,
+  sale: "<div>-10%</div>", 
+  style: "height: 110px; margin-top: 5px;"
 }];
 
 
@@ -30,13 +39,13 @@ product.forEach((product) => {
   productsHTML +=`
   <div class="square">
     <div class="product-image">
-      <img src="${product.image}" alt="">
+      <img style="${product.style}" src="${product.image}" alt="">
     </div>
     <div class="product-title">
       ${product.name}
     </div>
     <div class="product-price">
-      ${product.price} MDL <div>-30%</div>
+      ${product.price} MDL ${product.sale}
     </div>
     <div class="product-details">
       <button>Detalii</button>
