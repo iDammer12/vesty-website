5a5ca7d06b470dd8137338c77c00b83010ba1843
iDammer12 <146379225+iDammer12@users.noreply.github.com>
2023-10-22 10:59:05 +0300

    made it so that the slideshow dosent lag

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-let value = 1;
 
-function addOne() {
-  value += 1;
-  if(value === 6) {
-    value = 1
-  }
-  document.querySelector('.slideshow').innerHTML = `
-  <img class="slideshow-element" src="images/slideshow-products/${value}.jpg" alt="">
-  `
-}
-
-setInterval(addOne, 3000);
 
 const product = [{
   image: "images/jordan-1-black-orange.webp", 
-  name: "Air Jordan 1 Low", 
+  name: "Air Jordan 1 ", 
   price: 2199,
   sale: "<div>-30%</div>"
 }, {
@@ -55,3 +43,19 @@ product.forEach((product) => {
 })
 
 document.querySelector('.grid-container').innerHTML = productsHTML
+
+
+let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
