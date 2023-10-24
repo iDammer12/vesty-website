d6830fa3795ed6cf31a30f3f51fd42502b5d6efe
iDammer12 <146379225+iDammer12@users.noreply.github.com>
2023-10-24 16:37:14 +0300

    done 6 adidas shoe cells

--- js/script.js
+++ js/script.js
@@ -77,3 +77,69 @@ let slideIndex = 0;
         slides[slideIndex - 1].style.display = "block";
         setTimeout(showSlides, 3000); // Change image every 3 seconds
     }
+
+
+    const adidas = [{
+      image: "images/adidas-forum-low-white-brown.webp",
+      name: "Adidas Forum  ", 
+      price: 2299,
+      sale: "<div>-20%</div>"
+    }, {
+      image: "images/adidas-adifoam-black.webp", 
+      name: "Adidas Adifoam Q", 
+      price: 1699, 
+      sale: "<div>25%</div>",
+      style: "height: 70px; margin-top: -15px;  transform: scaleX(-1);"
+    }, {
+      image: "images/adidas-nizza-white.png", 
+      name: "Adidas Nizza ", 
+      price: 1699 ,
+      sale: "<div>-5%</div>", 
+      style: "height: 100px; margin-top: -5px; transform: scaleX(-1);"
+    }, {
+      image: "images/adidas-ultraboost-cream.webp", 
+      name: "Adidas UltraBoost ", 
+      price: 2599, 
+      sale: "<div></div>", 
+      style: "height: 65px; margin-top: 5px; "
+    }, {
+      image: "images/adidas-hoope-3-mid-black-white.webp", 
+      name: "Adidas Hoops 3", 
+      price: 1750, 
+      sale: "<div>10%</div>", 
+      style: "height: 50px; margin-top: 5px;"
+    }, {
+      style: "transform: scaleX(-1);", 
+      image: "images/adidas-duramo-sl.webp", 
+      name: "Adidas Duramo", 
+      price: 1399, 
+      sale:"<div>-15%</div>",
+      style: "height: 100px",
+      style: "transform: scaleX(-1); height:115px; margin-top: 15px",
+    }];
+    
+    
+    let adidasHTML = ''
+    
+    adidas.forEach((adidas) => {
+
+      adidasHTML +=`
+      <div class="square">
+        <div class="product-image">
+          <img style="${adidas.style}" src="${adidas.image}" alt="">
+        </div>
+        <div class="product-title">
+          ${adidas.name}
+        </div>
+        <div class="product-price">
+          ${adidas.price} MDL ${adidas.sale}
+        </div>
+        <div class="product-details">
+          <button>Detalii</button>
+        </div>
+      </div>
+      `
+      console.log(adidasHTML)
+    })
+    
+    document.querySelector('.adidas-grid-container').innerHTML = adidasHTML
